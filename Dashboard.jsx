2eba33299568a84aa32ecc84c2986fcc41e0e2fe
mayank gupta <151987610+mayankgupta8448@users.noreply.github.com>
2024-12-06 12:36:50 +0530

    Add files via upload

--- Dashboard.jsx
+++ Dashboard.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 function Dashboard() {
+  const [data, setData] = useState("");
+  function fetchData() {
+    fetch('https://api.github.com/users/Aatif09')
+      .then((response) => { return response.json() })
+      .then((dataa) => {
+        console.log(dataa);
+        setData(dataa);
+      });
+    console.log('Fetch Data Called');
+  }
   return (
-    <div>welcome to Dashboard</div>
+    <div>
+      <h2>Welcome to Dashboard Page</h2>
+      <h2>{data.login}</h2>
+      <h2>{data.name}</h2>
+      <h2>{data.bio}</h2>
+      <img src={data.avatar_url} alt="avatar" />
+      <button onClick={fetchData}>Click to see Your Git</button>
+    </div>
   )
 }
 
